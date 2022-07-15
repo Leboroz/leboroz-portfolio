abd86ca727dfd0343417ac74cf13e8acebd90a08
Leonardo Albornoz <albornozleo95@gmail.com>
2022-07-15 00:11:02 -0400

    Add ids to sections

---
+++ src/pages/AboutSection.tsx
@@ -0,0 +1,31 @@
+export default function AboutSection() {
+  return (
+    <section id="about" className="about-section section flex a-center">
+      <article className="about-section-content text-white flex column gap-1">
+        <h2 className="letter-gap weight-300 fs-3">About</h2>
+        <p className="fs-2">
+          Hello! My name is Leonardo and I enjoy creating things that live on the internet. Having me in your team means that you will not have to worry about any deadline. 
+        </p>
+        <p className="fs-2">
+          I’ve had the privilege of working with developers from all around the world building responsive websites ready to scale. My main focus is to build accessible, inclusive products and digital experiences.
+        </p>
+        <p className="fs-2">
+          Here are a few technologies I’ve been working with recently:
+        </p>
+        <div className="flex gap-5">
+          <ul>
+            <li>JavaScript (ES6+)</li>
+            <li>TypeScript</li>
+            <li>React</li>
+          </ul>
+          <ul>
+            <li>Ruby</li>
+            <li>Ruby on Rails</li> 
+            <li>Node.js</li>
+          </ul>
+        </div>
+      </article>
+      <div></div>
+    </section>
+  );
+}
