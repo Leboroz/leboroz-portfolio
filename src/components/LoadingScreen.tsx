8627f96d72f1b71d2bffffb65f285deb7bfdfe1c
Leonardo Albornoz <albornozleo95@gmail.com>
2022-07-18 15:04:45 -0400

    Add files

---
+++ src/components/LoadingScreen.tsx
@@ -0,0 +1,8 @@
+export default function LoadingScreen() {
+  return (
+    <div className="loading-screen">
+      <div className="circle circle-1"></div>
+      <div className="circle circle-2"></div>
+    </div>
+  );
+}
